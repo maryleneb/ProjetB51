f82b22df14d463d4778312c8bac30c8bab8510a5
Voyer-Poitras Simon <0980234@decinfo.cvm>
2018-10-31 11:42:56 -0400

    genererTables + inserts scalablee + select framework

--- Saas/gestpro/BaseDonnees.py
+++ Saas/gestpro/BaseDonnees.py
@@ -4,53 +4,54 @@ class  BaseDonnees():
     def __init__(self):
         self.connecteur = sqlite3.connect('SAAS.db')
         self.curseur = self.connecteur.cursor()
-        #self.creerTables()
-        self.creerTablesPrototype()
-        self.insertion()
-        self.selection()
+        self.creerTables(self.genererListeTables())
+        self.insertion("stocks", ['date','trans', 'symbol',53.2,5.2])
+        self.selection('Select * FROM stocks ')
         self.connecteur.close()
-        
-        
-        
-    def creerTables(self):
-        listeTables = ['stocks', ['date','text'], ['trans','text'], ['symbol','text'], ['qty','real'], ['price','real']]
-        try:
-            self.curseur.execute("DROP TABLE stocks")
-        except:
-            pass
-        finally:
-            stringBD = '''CREATE TABLE ''' +  listeTables[0] +''' ('''+listeTables[1][0]+listeTables[1][1]+''',''' +listeTables[2][0]+listeTables[2][1]+''',''' +listeTables[3][0]+listeTables[3][1]+''',''' +listeTables[4][0]+listeTables[4][1]+''',''' +listeTables[5][0]+listeTables[5][1]+''')'''
-            self.curseur.execute(stringBD)
     
-    def creerTablesPrototype(self):
+    def genererListeTables(self):
         listeTables = [ 
             ['stocks', ['date','text'], ['trans','text'], ['symbol','text'], ['qty','real'], ['price','real']],
             ['banane', ['date','text'], ['type','text'], ['symbol','text'], ['qty','real'], ['price','real']]
             ]
+        return listeTables
+        
+    def creerTables(self, listeTables):
         try:
-            self.curseur.execute("DROP TABLE stocks")
-            self.curseur.execute("DROP TABLE banane")
+            for table in listeTables:
+                stringDropTable = "DROP TABLE "
+                stringDropTable += table[0]
+                self.curseur.execute(stringDropTable)
         except:
             pass
         finally:
             for table in listeTables:
-                stringBD = "CREATE TABLE " + table[0] + "(" 
+                stringCreate = "CREATE TABLE " + table[0] + "(" 
                 for indiceEntrees in range(len(table)):
                     if indiceEntrees > 0:
-                        stringBD +=  table[indiceEntrees][0] + " " + table[indiceEntrees][1]
+                        stringCreate +=  table[indiceEntrees][0] + " " + table[indiceEntrees][1]
                         if indiceEntrees < len(table)-1:
-                            stringBD += ", "
-                stringBD += ")"
-                self.curseur.execute(stringBD)
+                            stringCreate += ", "
+                stringCreate += ")"
+                self.curseur.execute(stringCreate)
                 
-        
-   
-            
-    def insertion(self):
-        self.curseur.execute("INSERT INTO stocks VALUES('text1','text2', 'BMO', 50, 100)")
-        
-    def selection(self):
-        for rangee in self.curseur.execute('Select * FROM stocks '):
+    
+    def insertion(self, nomTable = "", listeValeurs=[]):
+        stringInsert = "INSERT INTO " + nomTable + " VALUES("
+        for indiceEntree in range(len(listeValeurs)):
+            if isinstance(listeValeurs[indiceEntree],str):
+                listeValeurs[indiceEntree] = "'"+listeValeurs[indiceEntree]+"'"
+                stringInsert += listeValeurs[indiceEntree]
+            else:
+                stringInsert += str(listeValeurs[indiceEntree])
+            if indiceEntree < len(listeValeurs)-1:
+                stringInsert += ", "
+        stringInsert += ")"
+        self.curseur.execute(stringInsert)
+    
+    
+    def selection(self, stringSelect = ""):
+        for rangee in self.curseur.execute(stringSelect):
             print(rangee)
            
         
